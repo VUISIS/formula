6536ccba19197830ea82ef5474903c26861d2a73
saj122 <sajttu22@gmail.com>
2023-02-03 17:51:08 -0600

    Added folder selection and treeview panel.

--- Src/Debugger/App.axaml.cs
+++ Src/Debugger/App.axaml.cs
@@ -2,6 +2,8 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
+using FormulaDebuggerGUI.Views;
+
 namespace FormulaDebuggerGUI;
 
 public partial class App : Application
--- Src/Debugger/Views/MainWindow.axaml.cs
+++ Src/Debugger/Views/MainWindow.axaml.cs
@@ -1,30 +1,99 @@
+using System;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Avalonia;
 using ReactiveUI;
 using Avalonia.Markup.Xaml;
 using Avalonia.Controls;
-using FormulaDebugger;
+using Avalonia.Platform.Storage;
+using Avalonia.VisualTree;
 
-using System.Threading.Tasks;
-
-namespace FormulaDebuggerGUI;
+namespace FormulaDebuggerGUI.Views;
 
 internal class MainWindowViewModel : ReactiveObject
 {
     private MainWindow _win;
-
+    private IStorageFolder? lastSelectedDirectory = null;
+    private Node _root = new Node("null");
+    
     public MainWindowViewModel(MainWindow win)
     {
         _win = win;
+        
+        Items = new ObservableCollection<Node>();
+        ItemsSource = new ObservableCollection<Node>();
+        SelectedItems = new ObservableCollection<Node>();
     }
+    
+    public ObservableCollection<Node>? Items { get; }
+    public ObservableCollection<Node>? ItemsSource { get; set;  }
+    public ObservableCollection<Node>? SelectedItems { get; }
 
-    public async void AttachCmd() 
+    public async void OpenCmd()
     {
-        var dialog = new ProcessListWindow();
-        await dialog.ShowDialog(_win);
+        await Task.Delay(2000);
     }
+    
+    private IStorageProvider GetStorageProvider()
+    {
+        return GetTopLevel().StorageProvider;
+    }
+    
+    TopLevel GetTopLevel() => _win.GetVisualRoot() as TopLevel ?? throw new NullReferenceException("Invalid Owner");
 
+    public async void OpenFolderCmd()
+    {
+        var folders = await GetStorageProvider().OpenFolderPickerAsync(new FolderPickerOpenOptions()
+        {
+            Title = "Folder",
+            SuggestedStartLocation = lastSelectedDirectory,
+            AllowMultiple = false
+        });
+
+        lastSelectedDirectory = folders.FirstOrDefault();
+        if (lastSelectedDirectory != null)
+        {
+            _root = new Node(lastSelectedDirectory.Name);
+            if(Items != null)
+                Items.Add(_root);
+            var items = await lastSelectedDirectory.GetItemsAsync();
+            foreach (var file in items)
+            {
+                var child = new Node(_root, file.Name);
+                _root.AddItem(child);
+            }
+
+            ItemsSource = _root.Children;
+        }
+    }
+    
     public void ExitCmd() 
     {
         _win.Close();
+    }
+    
+    public class Node
+    {
+        public Node(string folderName)
+        {
+            Header = folderName;
+            Children = new ObservableCollection<Node>();
+        }
+
+        public Node(Node parent, string fileName)
+        {
+            Children = new ObservableCollection<Node>();
+            Parent = parent;
+            Header = fileName;
+        }
+
+        public Node? Parent { get; }
+        public string Header { get; }
+        public ObservableCollection<Node> Children { get; }
+        public void AddItem(Node child) => Children.Add(child);
+        public void RemoveItem(Node child) => Children.Remove(child);
+        public override string ToString() => Header;
     }
 }
 
@@ -33,6 +102,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         this.InitializeComponent();
+        
+        this.AttachDevTools();
 
         this.DataContext = new MainWindowViewModel(this);
     }
