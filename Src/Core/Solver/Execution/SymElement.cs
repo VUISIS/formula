ac6b5dd5568e068bcd23adceecc478706175f558
Daniel Balasubramanian <danielbala@gmail.com>
2023-02-21 10:57:44 -0600

    Check for null before creating constraint

--- Src/Core/Solver/Execution/SymElement.cs
+++ Src/Core/Solver/Execution/SymElement.cs
@@ -129,7 +129,10 @@
                         executer.GetSymbolicTerm(posTerm, out next))
                     {
                         var nextConstraint = next.GetSideConstraints(executer, localProcessed);
-                        currConstraint = CreateAndCacheConstraint(executer, currConstraint, nextConstraint);
+                        if (nextConstraint != null)
+                        {
+                            currConstraint = CreateAndCacheConstraint(executer, currConstraint, nextConstraint);
+                        }
                     }
                 }
 
