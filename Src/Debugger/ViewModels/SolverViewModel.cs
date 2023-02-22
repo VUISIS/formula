c9659a778fd5f0f92fa5fbc51c53b0008ddadd23
Abdullah Nassar <abdullah.u.nassar@vanderbilt.edu>
2023-02-22 13:18:46 -0600

    Added windows for the solver panel

--- Src/Debugger/ViewModels/SolverViewModel.cs
+++ Src/Debugger/ViewModels/SolverViewModel.cs
@@ -1,4 +1,11 @@
 using ReactiveUI;
+using System.Collections.ObjectModel;
+
+using Debugger.Views;
+using Debugger.ViewModels.Types;
+using Debugger.Windows;
+using Microsoft.Formula.API;
+using Avalonia.Interactivity;
 
 namespace Debugger.ViewModels;
 
@@ -6,6 +13,21 @@ internal class SolverViewModel : ReactiveObject
 {
     public SolverViewModel()
     {
+        SymbolicVariablesItems = new ObservableCollection<Node>();
+        AllConstraintsItems = new ObservableCollection<Node>();
+        SolutionItems = new ObservableCollection<Node>();
 
     }
+
+    public void ClearAll()
+    {
+        SymbolicVariablesItems.Clear();
+        AllConstraintsItems.Clear();
+        SolutionItems.Clear();
+    }
+
+    public ObservableCollection<Node> SymbolicVariablesItems { get; }
+    public ObservableCollection<Node> AllConstraintsItems { get; }
+    public ObservableCollection<Node> SolutionItems { get; }
+    public string AddedConstraint { get; }
 }
