ae4d3d6e180cd5737b96fda7475212cee401bbb7
Stephen Johnson <sajttu22@gmail.com>
2023-07-10 17:01:03 -0500

    Moved CommandLine folder and updated the execute request function.

--- Src/Kernel/FormulaPy/formula_kernel/kernel.py
+++ Src/Kernel/FormulaPy/formula_kernel/kernel.py
@@ -5,9 +5,9 @@ from .utils import cmd_regex
 
 import os
 from pythonnet import load
-load("coreclr", runtime_config=os.path.abspath('./CommandLine/runtimeconfig.json'))
+load("coreclr", runtime_config=os.path.abspath('./formula_kernel/CommandLine/runtimeconfig.json'))
 import clr
-clr.AddReference(os.path.abspath('./CommandLine/CommandLine.dll'))
+clr.AddReference(os.path.abspath('./formula_kernel/CommandLine/CommandLine.dll'))
 
 from Microsoft.Formula.CommandLine import CommandInterface, CommandLineProgram
 from System.IO import StringWriter
@@ -35,6 +35,7 @@ class FormulaKernel(Kernel):
         chooser = CommandLineProgram.ConsoleChooser()
         self.ci = CommandInterface(sink, chooser)
         self.ci.DoCommand("wait on")
+        self.sw.GetStringBuilder().Clear()
     
     def load(self, path):
         return self.ci.DoCommand("load " + path)
@@ -42,7 +43,7 @@ class FormulaKernel(Kernel):
     def help(self):
         output = "Formula python commands:\n"
         output += "  loadf(path) - Loads and compiles a file that is not yet loaded. Use: load filepath.\n"
-        output += "  helpf() - Prints this message."
+        output += "  helpf()     - Prints this message."
         return output
 
     def send_command_output(self, silent, command_name, data):
@@ -77,19 +78,18 @@ class FormulaKernel(Kernel):
         for function in functions:
             match = re.search(cmd_regex, function)
             if match:
-                type, params, _ = match.groups()
-                if not type is None:
+                type, params = match.groups()
+                if type:
                     match type:
-                        case 'load':
+                        case 'loadf':
                             if params:
                                 param = params.split(",")
-                                if not os.path.isabs(param[0]):
-                                    param[0] = os.path.abspath(param[0])
-                                
+                                param[0] = re.sub('\"', '', param[0])
                                 if os.path.isfile(param[0]):
                                     status = self.load(param[0].strip())
                                     if status:
                                         self.send_command_output(silent, type, self.sw.ToString())
+                                        self.sw.GetStringBuilder().Clear()
                                         return_status = 'ok'
                                     else:
                                         return_status = 'error'
@@ -97,10 +97,12 @@ class FormulaKernel(Kernel):
                                     return_status = 'error'
                             else:
                                 return_status = 'error'
-                        case 'help':
+                        case 'helpf':
                             status = self.help()
                             self.send_command_output(silent, type, status)  
                             return_status = 'ok'
+            else:
+                self.send_command_output(silent, "NONE", "No matching command found.")
         return {    
             'status': return_status,
             'execution_count': self.execution_count,
--- Src/Kernel/FormulaPy/formula_kernel/utils.py
+++ Src/Kernel/FormulaPy/formula_kernel/utils.py
@@ -1,3 +1,3 @@
-commands = ["load", "help"]
+commands = ["loadf", "helpf"]
 
-cmd_regex = "(" + "|".join(commands) + ")\((\w(\,\w)*)?\)"
+cmd_regex = "(" + "|".join(commands) + ")\(([\"\w\/\.\,]+)?\)"
