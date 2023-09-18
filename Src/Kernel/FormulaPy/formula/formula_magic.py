e471f8dd7b9c092b393a9d14325286f87f433287
Stephen Johnson <sajttu22@gmail.com>
2023-09-18 16:07:34 -0500

    Cleaned up code and added repair magic command.

--- Src/Kernel/FormulaPy/formula/formula_magic.py
+++ Src/Kernel/FormulaPy/formula/formula_magic.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from IPython.core.magic import (Magics, magics_class, line_magic)
 
+from pathlib import Path
+
 from Microsoft.Formula.CommandLine import CommandInterface, CommandLineProgram
 from System.IO import StringWriter
 from System import Console
@@ -12,11 +14,12 @@ class FormulaMagics(Magics):
 
     def __init__(self, shell, data=None):
         super(FormulaMagics, self).__init__(shell)
-        self.loaded = False
         self.sw = StringWriter()
         self.total_sw_output = ""
         Console.SetOut(self.sw)
         Console.SetError(self.sw)
+        self.file = None
+        self.explain = False
 
         sink = CommandLineProgram.ConsoleSink()
         chooser = CommandLineProgram.ConsoleChooser()
@@ -26,91 +29,95 @@ class FormulaMagics(Magics):
         print("Successfully initialized formula.")
 
     @line_magic
-    def help(self, line):
+    def help(self):
         print("Formula magic commands:")
-        print("\t%load filepath - Loads and compiles a file that is not yet loaded. Use: load filepath.")
-        print("\t%help          - Prints this message.")
-        print("\t%list          - Lists environment objects. Use: ls [vars | progs | tasks]")
-        print("\t%extract       - Extract and install a result. Use: extract (app_id | solv_id n) output_name [render_class render_dll]")
-        print("\t%query         - Start a query task. Use: query model goals")
-        print("\t%solve         - Start a solve task. Use: solve partial_model max_sols goals")
-        print("\t%explain       - Runs an LLM to generate an explaination for the solution.")
-        print("\t%set           - Sets a variable. Use: set var term.")
-        print("\t%delete        - Deletes a variable. Use: del var.")
-        print("\t%save          - Saves the module modname into file.")
-        print("\t%unload        - Unloads an installed program and all dependent programs. Use: unload [prog | *]")
-        print("\t%tunload       - Unloads a task. Use: tunload [id | *]")
-        print("\t%reload        - Reloads an installed program and all dependent programs. Use: reload [prog | *]")
-        print("\t%print         - Prints the installed program with the given name. Use: print progname")
-        print("\t%det           - Prints details about the compiled module with the given name. Use: det modname")
-        print("\t%types         - Prints inferred variable types. Use: types modname")
-        print("\t%render        - Tries to render the module. Use: render modname")
-        print("\t%verbose       - Changes verbosity. Use: verbose (on | off)")
-        print("\t%wait          - Changes waiting behavior. Use: wait (on | off) to block until task completes")
-        print("\t%apply         - Start an apply task. Use: apply transformstep")
-        print("\t%stats         - Prints task statistics. Use: stats task_id [top_k_rule]")
-        print("\t%generate      - Generate C# data model. Use: generate modname")
-        print("\t%truth         - Test if a ground term is derivable under a model/apply. Use: truth task_id [term]")
-        print("\t%proof         - Enumerate proofs that a ground term is derivable under a model/apply. Use: proof task_id [term]")
-        print("\t%confhelp      - Provides help about module configurations and settings")
-        print("\t%watch         - Use: watch [off | on | prompt] to control watch behavior")
-        print("\t%core          - Prints reduced rule set for domains / transforms. Use: core module_name")
-        print("\t%downgrade     - Attempts to downgrade a (partial) model to Formula V1. Use: downgrade module_name")
+        print("%load filepath - Loads and compiles a file that is not yet loaded. Use: load filepath.")
+        print("%help          - Prints this message.")
+        print("%ls            - Lists environment objects. Use: ls [vars | progs | tasks]")
+        print("%extract       - Extract and install a result. Use: extract (app_id | solv_id n) output_name [render_class render_dll]")
+        print("%query         - Start a query task. Use: query model goals")
+        print("%solve         - Start a solve task. Use: solve partial_model max_sols goals")
+        print("%explain       - Runs an LLM to generate an explaination for the solution.")
+        print("%repair        - Runs an LLM to repair the loaded formula DSL program.")
+        print("%set           - Sets a variable. Use: set var term.")
+        print("%delete        - Deletes a variable. Use: del var.")
+        print("%save          - Saves the module modname into file.")
+        print("%unload        - Unloads an installed program and all dependent programs. Use: unload [prog | *]")
+        print("%tunload       - Unloads a task. Use: tunload [id | *]")
+        print("%reload        - Reloads an installed program and all dependent programs. Use: reload [prog | *]")
+        print("%print         - Prints the installed program with the given name. Use: print progname")
+        print("%det           - Prints details about the compiled module with the given name. Use: det modname")
+        print("%types         - Prints inferred variable types. Use: types modname")
+        print("%render        - Tries to render the module. Use: render modname")
+        print("%verbose       - Changes verbosity. Use: verbose (on | off)")
+        print("%wait          - Changes waiting behavior. Use: wait (on | off) to block until task completes")
+        print("%apply         - Start an apply task. Use: apply transformstep")
+        print("%stats         - Prints task statistics. Use: stats task_id [top_k_rule]")
+        print("%generate      - Generate C# data model. Use: generate modname")
+        print("%truth         - Test if a ground term is derivable under a model/apply. Use: truth task_id [term]")
+        print("%proof         - Enumerate proofs that a ground term is derivable under a model/apply. Use: proof task_id [term]")
+        print("%confhelp      - Provides help about module configurations and settings")
+        print("%watch         - Use: watch [off | on | prompt] to control watch behavior")
+        print("%core          - Prints reduced rule set for domains / transforms. Use: core module_name")
+        print("%downgrade     - Attempts to downgrade a (partial) model to Formula V1. Use: downgrade module_name")
 
-    @line_magic
-    def load(self, line):
-        self.file = line
-        self.ci.DoCommand("load " + line)
-        self.total_sw_output += "load " + line + "\n"
+    def run_command(self, cmd, args=None):
+        line = ""
+        if args == None:
+            line = cmd
+        else:
+            line = cmd + " " + args
+            
+        self.ci.DoCommand(line)
+        
         temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
-        print('hello world')
-        self.loaded = True
-
-
-    @line_magic
-    def list(self, line):
-        self.ci.DoCommand("ls")
-        self.total_sw_output += "ls"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
-
-    @line_magic
-    def extract(self, line):
-        self.ci.DoCommand("extract " + line)
-        self.total_sw_output += "extract " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
+        if cmd == "extract":
+            self.total_sw_output += temp_str
+        elif cmd == "load" and self.file != None: 
+            f = open(self.file.absolute(), 'r')
+            self.total_sw_output += f.read()
+            f.close()
         self.sw.GetStringBuilder().Clear()
         print(temp_str)
 
     @line_magic
-    def query(self, line):
-        self.ci.DoCommand("query " + line)
-        self.total_sw_output += "query " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
+    def load(self, line=None):
+        if line == None:
+            print("No 4ml file passed to load.")
+            return
+        
+        file = Path(line)
+        if file.is_file():
+            self.run_command("load", file.absolute())
+            self.file = file
+            return
+        
+        print("File is invalid or does not exist.")
 
     @line_magic
-    def solve(self, line):
-        if not self.loaded:
+    def ls(self):
+        self.run_command("ls")
+
+    @line_magic
+    def extract(self, line=None):
+        self.run_command("extract", line)
+
+    @line_magic
+    def query(self, line=None):
+        self.run_command("query", line)
+
+    @line_magic
+    def solve(self, line=None):
+        if self.file == None:
             print("Load 4ml file to proceed.")
-        self.ci.DoCommand("solve " + line)
-        self.total_sw_output += "solve " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+            return
+        self.run_command("solve", line)
 
     @line_magic
-    def explain(self, line):
-        if not self.loaded:
+    def explain(self, line=None):
+        if self.file == None:
             print("Load 4ml file to proceed.")
+            return
         f = open(self.file, 'r')
         txt = ""
         try:
@@ -120,194 +127,100 @@ class FormulaMagics(Magics):
 
         print('Running executor...')
         run_agent_executor(txt, self.total_sw_output, line)
+        self.explain = True
+        
+    @line_magic
+    def repair(self):
+        if self.explain:
+            print("Repair")
+        else:
+            print("Run explain first")
 
     @line_magic
-    def set(self, line):
-        self.ci.DoCommand("set " + line)
-        self.total_sw_output += "set " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def explain_and_repair(self):
+        print("Explain and repair.")
 
     @line_magic
-    def delete(self, line):
-        self.ci.DoCommand("del " + line)
-        self.total_sw_output += "del " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def set(self, line=None):
+        self.run_command("set", line)
 
     @line_magic
-    def unload(self, line):
-        self.ci.DoCommand("unload " + line)
-        self.total_sw_output += "unload " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
-        self.loaded = False
-        print("Remember to load a file before proceeding.")
+    def delete(self, line=None):
+        self.run_command("del", line)
 
     @line_magic
-    def tunload(self, line):
-        self.ci.DoCommand("tunload " + line)
-        self.total_sw_output += "tunload " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def unload(self, line=None):
+        self.file = None
+        self.run_command("unload", line)
 
     @line_magic
-    def reload(self, line):
-        self.ci.DoCommand("reload")
-        self.total_sw_output += "reload\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def tunload(self, line=None):
+        self.run_command("tunload", line)
 
     @line_magic
-    def save(self, line):
-        self.ci.DoCommand("save " + line)
-        self.total_sw_output += "save " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def reload(self):
+        self.run_command("reload")
 
     @line_magic
-    def print(self, line):
-        self.ci.DoCommand("print " + line)
-        self.total_sw_output += "print " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def save(self, line=None):
+        self.run_command("save", line)
 
     @line_magic
-    def det(self, line):
-        self.ci.DoCommand("det " + line)
-        self.total_sw_output += "det " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def print(self, line=None):
+        self.run_command("print", line)
 
     @line_magic
-    def render(self, line):
-        self.ci.DoCommand("render " + line)
-        self.total_sw_output += "render " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def det(self, line=None):
+        self.run_command("det", line)
 
     @line_magic
-    def verbose(self, line):
-        self.ci.DoCommand("verbose " + line)
-        self.total_sw_output += "verbose " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def render(self, line=None):
+        self.run_command("render", line)
 
     @line_magic
-    def wait(self, line):
-        self.ci.DoCommand("wait " + line)
-        self.total_sw_output += "wait " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def verbose(self, line=None):
+        self.run_command("verbose", line)
 
     @line_magic
-    def watch(self, line):
-        self.ci.DoCommand("watch " + line)
-        self.total_sw_output += "watch " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def wait(self, line=None):
+        self.run_command("wait", line)
 
     @line_magic
-    def types(self, line):
-        self.ci.DoCommand("types " + line)
-        self.total_sw_output += "types " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def watch(self, line=None):
+        self.run_command("watch", line)
 
     @line_magic
-    def truth(self, line):
-        self.ci.DoCommand("truth " + line)
-        self.total_sw_output += "truth " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def types(self, line=None):
+        self.run_command("type", line)
 
     @line_magic
-    def proof(self, line):
-        self.ci.DoCommand("proof " + line)
-        self.total_sw_output += "proof " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def truth(self, line=None):
+        self.run_command("truth", line)
 
     @line_magic
-    def apply(self, line):
-        self.ci.DoCommand("apply " + line)
-        self.total_sw_output += "apply " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def proof(self, line=None):
+        self.run_command("proof", line)
+
+    @line_magic
+    def apply(self, line=None):
+        self.run_command("apply", line)
     
     @line_magic
-    def stats(self, line):
-        self.ci.DoCommand("stats " + line)
-        self.total_sw_output += "stats " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def stats(self, line=None):
+        self.run_command("stats", line)
 
     @line_magic
-    def generate(self, line):
-        self.ci.DoCommand("generate " + line)
-        self.total_sw_output += "generate " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def generate(self, line=None):
+        self.run_command("generate", line)
 
     @line_magic
-    def confhelp(self, line):
-        self.ci.DoCommand("confhelp " + line)
-        self.total_sw_output += "confhelp " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def confhelp(self, line=None):
+        self.run_command("confhelp", line)
 
     @line_magic
-    def core(self, line):
-        self.ci.DoCommand("core " + line)
-        self.total_sw_output += "core " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def core(self, line=None):
+        self.run_command("core", line)
 
     @line_magic
-    def downgrade(self, line):
-        self.ci.DoCommand("downgrade " + line)
-        self.total_sw_output += "downgrade " + line + "\n"
-        temp_str = self.sw.ToString()
-        self.total_sw_output += temp_str
-        self.sw.GetStringBuilder().Clear()
-        print(temp_str)
+    def downgrade(self, line=None):
+        self.run_command("downgrade", line)
